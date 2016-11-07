c6340a00b2214dfef4e1a6ee13380e432019372e
Joshua Yang <tianjyang@gmail.com>
2016-11-06 16:18:39 -0800

    basic implementation of a linked list in javascript

--- JavaScript/linkedlist.js
+++ JavaScript/linkedlist.js
@@ -1,11 +1,60 @@
 class Node {
   constructor(value){
     this.next = null;
-    this.prev = null;
-    this.val = value
+    // this.prev = null;
+    this.value = value;
   }
 }
 
-console.log("hi there!");
-let b = new Node;
-console.log(b);
+class LinkedList {
+  constructor(){
+    this.head = null;
+    this.tail = null;
+  }
+
+  printList(){
+    let node = this.head;
+    while (node) {
+      console.log(node.value);
+      node = node.next;
+    }
+  }
+
+  addLink(value){
+    if (!this.head) {
+      this.head = new Node(value);
+      this.tail = this.head;
+    } else {
+      let newLink = new Node(value);
+      this.tail.next = newLink;
+      this.tail = newLink;
+    }
+  }
+
+  find(value){
+    let node = this.head;
+    while (node) {
+      if (node.value===value){
+        return node;
+      }
+      node = node.next;
+    }
+    return null;
+  }
+
+  delete(value) {
+    let node = this.head;
+    let nodeToDelete = node.next;
+    while (nodeToDelete) {
+      if (nodeToDelete.value===value){
+        node.next = nodeToDelete.next;
+      }
+      node = node.next;
+      nodeToDelete = nodeToDelete.next;
+      return true;
+    }
+    return null;
+  }
+}
+
+module.exports = LinkedList;
