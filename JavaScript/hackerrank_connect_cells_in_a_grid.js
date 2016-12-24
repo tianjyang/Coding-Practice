8868de49e9a3641820e09b6f8321b03bc572021f
Joshua Yang <tianjyang@gmail.com>
2016-12-24 14:27:43 -0800

    add hackerrank cells in a grid problem

--- JavaScript/hackerrank_connect_cells_in_a_grid.js
+++ JavaScript/hackerrank_connect_cells_in_a_grid.js
@@ -6,11 +6,11 @@ const findRegionSize = (coord, grid, count) => {
   }
   let xCoord = coord[0];
   let yCoord = coord[1];
-  if (xCoord > grid.length || xCoord < 0) {
+  if (xCoord >= grid.length || xCoord < 0) {
     return count;
   }
 
-  if (yCoord > grid[0].length || xCoord < 0) {
+  if (yCoord >= grid[0].length || xCoord < 0) {
     return count;
   }
   if (grid[coord[0]][coord[1]]) {
@@ -22,21 +22,20 @@ const findRegionSize = (coord, grid, count) => {
     });
   }
   return count;
-
 };
 
-// let region = [[1,0,0],[1,1,0],[0,0,0]];
-// console.log("expect this test case to return 3");
-// console.log(findRegionSize([0,0],region));
-//
-// region = [[0,0,0],[1,1,0],[0,0,0]];
-// console.log("expect this test case to return 0");
-// console.log(findRegionSize([0,0],region));
-//
-// region = [[0,0,0],[1,1,0],[0,0,0]];
-// console.log("expect this test case to return 2");
-// console.log(findRegionSize([1,0],region));
-//
-// region = [[0,0,0],[1,1,0],[0,0,0]];
-// console.log("expect this test case to return 2");
-// console.log(findRegionSize([1,1],region));
+let region = [[1,0,0],[1,1,0],[0,0,0]];
+console.log("expect this test case to return 3");
+console.log(findRegionSize([0,0],region));
+
+region = [[0,0,0],[1,1,0],[0,0,0]];
+console.log("expect this test case to return 0");
+console.log(findRegionSize([0,0],region));
+
+region = [[0,0,0],[1,1,0],[0,0,0]];
+console.log("expect this test case to return 2");
+console.log(findRegionSize([1,0],region));
+
+region = [[0,0,0],[1,1,0],[0,0,0]];
+console.log("expect this test case to return 2");
+console.log(findRegionSize([1,1],region));
